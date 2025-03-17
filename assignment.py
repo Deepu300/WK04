ee9938c193360bf6546f80e651d23b28318318bf
Deepu300 <159274374+Deepu300@users.noreply.github.com>
2025-03-16 22:05:29 -0400

    Add Assignment python file

---
+++ assignment.py
@@ -0,0 +1,31 @@
+# pip install pillow torch
+
+import requests
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+# img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
+# raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
+# Provide the local image path
+image_path = "C:/Users/deepa/Documents/GitHub/WK04/student.png"  # Change this to your actual image path
+raw_image = Image.open(image_path).convert('RGB')
+
+
+
+# conditional image captioning
+text = "a photography of"
+inputs = processor(raw_image, text, return_tensors="pt")
+
+out = model.generate(**inputs)
+print(processor.decode(out[0], skip_special_tokens=True))
+# >>> a photography of a woman and her dog
+
+# unconditional image captioning
+inputs = processor(raw_image, return_tensors="pt")
+
+out = model.generate(**inputs)
+print(processor.decode(out[0], skip_special_tokens=True))
